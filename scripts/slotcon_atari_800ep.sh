d485a01be88f5b6261f29bd71a0f2b720447370b
Anastasiia Pedan <nastia.pedan1701@gmail.com>
2024-07-09 14:45:10 -0400

    added opt to change num prot-s in scripts

--- scripts/slotcon_atari_800ep.sh
+++ scripts/slotcon_atari_800ep.sh
@@ -7,9 +7,10 @@ arch=$1
 dim_out=$2
 dim_hidden=$3
 game=$4
+num_prots=$5
 
 data_dir="../atari_ds/${game}_128"
-output_dir="./output/slotcon_atari_${game}_${arch}_${dim_out}_${dim_hidden}_800ep"
+output_dir="./output/slotcon_atari_${game}_${arch}_${dim_out}_${dim_hidden}_${num_prots}_800ep"
 
 echo ${output_dir}
 
@@ -24,7 +25,7 @@ CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 \
     --arch ${arch} \
     --dim-hidden ${dim_hidden} \
     --dim-out ${dim_out} \
-    --num-prototypes 32 \
+    --num-prototypes ${num_prots} \
     --teacher-momentum 0.99 \
     --teacher-temp 0.07 \
     --group-loss-weight 0.5 \
